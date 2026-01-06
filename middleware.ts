5bba06b000121adb2331401c537b936e039525d2
Julio Olmedo <turlan125@gmail.com>
2026-01-06 13:34:25 -0300

    add instagram in middleware

--- middleware.ts
+++ middleware.ts
@@ -119,7 +119,7 @@ export function middleware(request: NextRequest) {
     "img-src 'self' data: https:; " +
     "font-src 'self' data:; " +
     "connect-src 'self' https://maps.googleapis.com https://*.googleapis.com; " +
-    "frame-src 'self' https://www.tiktok.com https://*.tiktok.com https://www.google.com;"
+    "frame-src 'self' https://www.tiktok.com https://*.tiktok.com https://www.google.com; https://*.instagram.com"
   )
 
   // Rate limit headers informativos
